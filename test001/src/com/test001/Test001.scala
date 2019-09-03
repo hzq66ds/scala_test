73a6f6e4e1ac5100a28f7b926ce2550c6b583377
韩志强 <x_hzq@sina.com>
2019-09-03 10:52:32 +0800

    初始化

---
+++ test001/src/com/test001/Test001.scala
@@ -0,0 +1,11 @@
+package com.test001
+
+/**
+ * Created by hanzhiqiang@lenztechretail.com 
+ * on 2019/9/3. 
+ */
+object Test001 {
+  def main(args: Array[String]): Unit = {
+    println("hello word")
+  }
+}
